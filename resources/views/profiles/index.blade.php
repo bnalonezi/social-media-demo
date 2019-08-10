592f86063641f12c2ec038b0e4cdc719938aac45
bnalonezi <b.alenezi@zid.sa>
2019-08-10 06:47:04 +0300

    add Post & Profile model ,migration ,controller

---
+++ resources/views/profiles/index.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="{{ asset('img/phpLogo.png') }}" class="rounded-circle" style="width: 200px">
+        </div>
+        <div class="col-9 p-5">
+            <div class="d-flex justify-content-between align-items-baseline">
+                <h1>{{ $user->username  }}</h1>
+                <a href="/p/create"> Add new post </a>
+            </div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>152</strong>Post</div>
+                <div class="pr-5"><strong>23K</strong>Followers</div>
+                <div class="pr-5"><strong>22</strong>Following</div>
+            </div>
+            <div class="pt-4 font-weight-bold">
+                {{ $user->profile->title  }}
+            </div>
+            <div>
+                {{ $user->profile->description  }}
+            </div>
+            <div>
+                {{ $user->profile->url ?? 'not url availavle'  }}
+            </div>
+        </div>
+    </div>
+
+
+    <div class="row pt-5">
+        @foreach($user->posts as $post)
+        <div class="col-4 pb-4">
+            <img src="/storage/{{$post->image}}" class="w-100">
+        </div>
+        @endforeach
+    </div>
+
+</div>
+@endsection
